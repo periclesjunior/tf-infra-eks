cf008ec1ce596906a293fdc6ffc51629e3038d7b
Pericles Junior <pericles_scj@gmail.com>
2026-04-18 13:16:13 -0300

    Refactoring locals.tf and main.tf

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -5,10 +5,4 @@ locals {
     Project       = "EKS"
     Environment   = "DEV"
   }
-
-  auto_scale_options = {
-    min     = 1
-    max     = 2
-    desired = 1
-  }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,14 @@
 module "eks" {
-  source                  = "git@github.com:periclesjunior/tf-infra-eks-modules.git?ref=main"
-  cidr_block              = "10.0.0.0/16"
-  project_name            = "nina-eks-001"
-  region                  = "us-east-1"
-  tags                    = local.tags
-  auto_scale_options      = local.auto_scale_options
+  source       = "git@github.com:periclesjunior/tf-infra-eks-modules.git?ref=main"
+  cidr_block   = "10.0.0.0/16"
+  project_name = "nina-eks-001"
+  region       = "us-east-1"
+  tags         = local.tags
+  auto_scale_options = {
+    min     = 1
+    max     = 2
+    desired = 1
+  }
   disk_size               = "150"
   ami_type                = "AL2023_x86_64_STANDARD"
   instance_types          = ["t3.medium"]
