ef91b0019bb898b3656bf98a45e7b2dcb5e76954
Pericles Junior <pericles_scj@gmail.com>
2026-04-18 11:38:24 -0300

    Add endpoints private and public access vars

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,15 @@
 module "eks" {
-  source             = "git@github.com:periclesjunior/tf-infra-eks-modules.git?ref=main"
-  cidr_block         = "10.0.0.0/16"
-  project_name       = "nina-eks-001"
-  region             = "us-east-1"
-  tags               = local.tags
-  auto_scale_options = local.auto_scale_options
-  disk_size          = "150"
-  ami_type           = "AL2023_x86_64_STANDARD"
-  instance_types     = ["t3.medium"]
-  capacity_type      = "ON_DEMAND"
-  cluster_version = "1.35"
+  source                  = "git@github.com:periclesjunior/tf-infra-eks-modules.git?ref=main"
+  cidr_block              = "10.0.0.0/16"
+  project_name            = "nina-eks-001"
+  region                  = "us-east-1"
+  tags                    = local.tags
+  auto_scale_options      = local.auto_scale_options
+  disk_size               = "150"
+  ami_type                = "AL2023_x86_64_STANDARD"
+  instance_types          = ["t3.medium"]
+  capacity_type           = "ON_DEMAND"
+  cluster_version         = "1.35"
+  endpoint_private_access = "true"
+  endpoint_public_access  = "true"
 }
